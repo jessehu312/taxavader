91a693a2ac17df485a20cd466e1132451b700c0a
jessehu312 <jessehu312@gmail.com>
2021-01-16 22:36:39 -0800

    added frontend and wired up backend

--- finance/__init__.py
+++ finance/__init__.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template
 from . import tax
 
+blueprint = Blueprint('home', __name__)
+
+@blueprint.route('/')
+def root():
+    return render_template('main.html')
+
+@blueprint.route('/<path:filepath>')
+def serve(filepath):
+    return send_from_directory('./app/static', filename=filepath)
+
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder='static', template_folder="static", static_url_path='')
 
     # a simple page that says hello
     @app.route('/hello')
@@ -10,5 +20,6 @@ def create_app():
         return 'Hello, World!'
     
     app.register_blueprint(tax.bp)
-
+    app.register_blueprint(blueprint)
     return app
+
